73ec4d090aa6e02a2842c8a233e39d65d9c47273
azure4-mac <brunoessingbarboza@gmail.com>
2024-10-25 20:54:45 -0300

    serv funcional, começo pg1

--- src/1paginaInicial/App.vue
+++ src/1paginaInicial/App.vue
@@ -1,151 +1,146 @@
 <template>
-  <section class="section1">
-    <h1 class="titulo1">BEM-VINDO AO</h1>
-    <h1 class="titulo2">NOSSO SITE</h1>
-    <p class="texto1">ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium
-      lacinia, dolor dui lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst.
-      Suspendisse eget orci elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat
-      dictum. Cras ac efficitur lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem
-      bibendum dui ornare cursus ut nec tortor. Curabitur vel erat quis diam semper</p>
-    <img src="Voyager1.jpg" class="imagem1">
-  </section>
-  <section class="section2">
-    <h1 class="titulo3">VOYAGERS: MISSÃO AO</h1>
-    <h1 class="titulo4">DESCONHECIDO</h1>
-    <img src="../assets/linha.jpg" class="imagem2">
-    <p class="texto2">
-      ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium lacinia, dolor dui
-      lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst. Suspendisse eget orci
-      elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur
-      lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut
-      nec tortor. Curabitur vel erat quis diam semper
-    </p>
-    <div>
-      <img src="../assets/Jupiter.jpg" class="imagem3">
-      <img src="../assets/Voyager2.jpg" class="imagem4">
-      <img src="../assets/DiscoOuro.jpg" class="imagem5">
-    </div>
+  <div>
+    <header>
+      <nav>
+        <a href="#" class="logo">
+          <img src="" alt="Ícone de um foguete" />
+        </a>
+        <ul>
+          <li><a href="#" class="active">Página Principal</a></li>
+          <li><a href="#">Linha do Tempo</a></li>
+          <li><a href="#">História</a></li>
+          <li><a href="#">Fórum</a></li>
+          <li><a href="#">Artigo</a></li>
+          <li><a href="#">Jogo</a></li>
+        </ul>
+        <a href="#" class="profile">
+          <img src="" alt="Ícone de um perfil" />
+        </a>
+      </nav>
+      <section class="hero">
+        <h1>Beyond<span>Galaxy</span></h1>
+        <p>Descubra um dos maiores projetos humanos para a exploração espacial</p>
+        <button>Saiba Mais</button>
+      </section>
+    </header>
 
-  </section>
+    <section class="intro">
+      <h3>BEM-VINDO AO <span>NOSSO SITE</span></h3>
+      <p>ALorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+      <img src="" alt="Imagem de uma das Sondas" />
+    </section>
+
+    <section class="topics">
+      <div>VOYAGERS: MISSÃO AO DESCONHECIDO</div>
+      <div>ALorem ipsum dolor sit amet, consectetur adipiscing elit...</div>
+      <img src="" alt="Imagem de Júpiter" />
+      <img src="" alt="Imagem de uma das sondas" />
+      <img src="" alt="Imagem do Disco de Ouro" />
+    </section>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'paginaInicial',
-  components: {
-
-  }
-}
+  name: 'App',
+  components: {}
+};
 </script>
 
-<style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: "Onest", sans-serif;
-    font-weight: 100;
+<style scoped>
+header {
+  height: 100vh;
+  background: url();
 }
 
-.section1 {
-    background-color: #080E22;
-    width: 100%;
-    padding-bottom: 90px;
-    display: flex; /* Flexbox para controle mais preciso */
-    flex-direction: column; /* Coloca o h1 em uma coluna */
-    align-items: flex-start; /* Mantém o alinhamento inicial */
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
 }
 
-
-.titulo1{
-    color: #F0F0FA;
-    font-size: 47px;
-    margin-top: 110px;
-    margin-left: 130px;
-    overflow: hidden;
-    font-family: "Onest", sans-serif;
+.logo img,
+.profile img {
+  height: 40px;
 }
 
-.titulo2{
-    color: #2552EA;
-    font-size: 47px;
-    margin-left: 130px;
-    overflow: hidden;
-}
-.texto1{
-    color: #F0F0FA;
-    font-size: 21px;
-    margin-top: 60px;
-    margin-left: 130px;
-    max-width: 680px;
-    overflow: hidden;
-}
-.imagem1{
-    width: 480px; 
-    height: auto;
-    margin-left: 830px;
-    margin-top: -360px;
-    overflow: hidden;
-    border-radius: 15px;
+nav ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
 }
 
-.section2 {
-    background-color: #111C41;
-    width: 100%;
-    padding-bottom: 90px;
-    display: flex; /* Flexbox para controle mais preciso */
-    flex-direction: column; /* Coloca o h1 em uma coluna */
-    align-items: flex-start; /* Mantém o alinhamento inicial */
+nav ul li {
+  margin: 0 15px;
 }
 
-.titulo3{
-    color: #F0F0FA;
-    font-size: 47px;
-    margin-top: 35px;
-    margin-left: 380px;
-    overflow: hidden;
-    text-align: center;
-}
-.titulo4{
-    color: #C898EC;
-    font-size: 47px;
-    margin-top: 10px;
-    margin-left: 460px;
-    overflow: hidden;
-    text-align: center;
-}
-.imagem2{
-    border-radius: 5px;
-    margin-top: 5px;
-    margin-left: 440px;
-}
-.texto2{
-    color: #F0F0FA;
-    font-size: 21px;
-    margin-top: 60px;
-    margin-left: 70px;
-    max-width: 1200px;
-    overflow: hidden;
-}
-.imagem3{
-    margin-top: 40px;
-    margin-left: 70px;
-    width: 450px;
-}
-.imagem4{
-    margin-top: 40px;
-    margin-left: 100px;
-    width: 270px;
-}
-.imagem5{
-    margin-top: 35px;
-    margin-left: 100px;
-    width: 270px;
+nav ul li a {
+  text-decoration: none;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
 }
 
-.div1{
-    display: flex;
-    flex-direction: row;
+nav ul li a:hover {
+  color: #C898EC;
+}
+
+nav a.active {
+  border-bottom: 3px solid #C898EC;
+  color: white;
+}
+
+.hero {
+  color: white;
+  text-align: center;
+  bottom: 10vh;
+}
+
+.hero button {
+  border-radius: 40px;
+  padding: 20px 40px;
+  background-color: transparent;
+  color: white;
+  border: 2px solid white;
+}
+
+.hero span {
+  text-decoration: solid underline #C898EC 4px;
+}
+
+.hero p {
+  text-align: center;
+}
+
+.intro {
+  background-color: #080E22;
+}
+
+.intro p {
+  color: white;
+  font-size: 1.5em;
+  padding: 10px;
+  width: 70%;
+  margin: 20px auto;
+  text-align: left;
+}
+
+.intro h3 {
+  color: white;
+  margin-left: 45px;
+  font-size: 50px ;
+}
+
+.intro span {
+  color: #2552EA;
+  text-align: end;
+}
+
+.intro img {
+  padding-right: 10px;
+  height: 497px;
+  width: 431px;
+  border-radius: 15%;
 }
 </style>
