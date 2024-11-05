4b046f32d04f228530e726c771e0cdd7f8b5cc1b
juschmidtt <juschmidt1302@gmail.com>
2024-11-05 14:51:36 -0300

    correção de margens, texto, fontes, codigo

--- src/1paginaInicial/App.vue
+++ src/1paginaInicial/App.vue
@@ -24,12 +24,14 @@
   </header>
 
   <div class="intro">
-    <h3>BEM-VINDO AO <span><br> NOSSO SITE</span></h3>
-    <p>ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium lacinia, dolor dui
-    lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst. Suspendisse eget orci
-    elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur
-    lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut
-    nec tortor. Curabitur vel erat quis diam semper</p>
+    <div class="txt">
+      <h3>BEM-VINDO AO <span><br> NOSSO SITE</span></h3>
+      <p>ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium lacinia, dolor dui
+      lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst. Suspendisse eget orci
+      elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur
+      lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut
+      nec tortor. Curabitur vel erat quis diam semper</p>
+    </div>
     <img src="../assets/Voyager1.jpg">
   </div>
 
@@ -40,9 +42,13 @@
     elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur
     lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut
     nec tortor. Curabitur vel erat quis diam semper.</p>
-    <img class='imagem1' src="../assets/Jupiter.jpg"/>
-    <img class="imagem2" src="../assets/voyager2.png"/>
-    <img class="imagem3" src="../assets/DiscoOuro.jpg"/>
+
+    <div class="imagens">
+      <img class='imagem1' src="../assets/Jupiter.jpg"/>
+      <img class="imagem2" src="../assets/voyager2.png"/>
+      <img class="imagem3" src="../assets/DiscoOuro.jpg"/>
+    </div>
+
     <h3>A Grande Conjunção</h3>
     <p>ALorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, arcu nec pretium lacinia, dolor dui lobortis nunc, sed condimentum libero orci ut odio. In hac habitasse platea dictumst. Suspendisse eget orci elementum, porttitor turpis sed, ultrices dui. Vestibulum consequat augue ac mi feugiat dictum. Cras ac efficitur lorem, in ultricies tellus. Duis accumsan nulla eu pretium mattis. Vestibulum ac sem bibendum dui ornare cursus ut nec tortor. Curabitur vel erat quis diam semper</p>
     <button><a>Veja mais</a></button>
@@ -59,111 +65,108 @@ export default {
 
 <style scoped>
 
-  .intro {
-    background-color: #080E22;
-    width: 100%;
-    padding-bottom: 90px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-  }
+.intro {
+  background-color: #080E22;
+  width: 100%;
+  padding-bottom: 90px;
+  display: flex;
+}
 
-  .intro p {
-    color: #F0F0FA;
-    font-size: 21px;
-    margin-top: 2%;
-    margin-left: 10%;
-    max-width: 600px;
-    overflow: hidden;
-  }
+.txt {
+  width: 60%;
+}
 
-  .intro h3 {
-    color: #F0F0FA;
-    font-size: 47px;
-    margin-top: 6%;
-    margin-left: 10%;
-    overflow: hidden;
-    font-family: "Onest", sans-serif;
-  }
+.intro p {
+  color: #F0F0FA;
+  font-size: 21px;
+  margin-top: 2%;
+  margin-left: 10%;
+  max-width: 600px;
+  font-weight: 300;
+}
 
-  .intro span {
-    color: #2552EA;
-  }
+.intro h3 {
+  color: #F0F0FA;
+  font-size: 47px;
+  margin-top: 6%;
+  margin-left: 10%;
+}
 
-  .intro img {
-    width: 40%;
-    height: auto;
-    margin-left: 75%;
-    margin-top: -30%;
-    border-radius: 15px;
-    object-fit: contain;
-    transform: translateX(-50%);
-  }
+.intro span {
+  color: #2552EA;
+}
 
-  .topics {
-    background-color: #111C41;
-    width: 100%;
-    padding-bottom: 90px;
-    display: flex; 
-    flex-direction: column; 
-    align-items: flex-start; 
-  }
+.intro img {
+  width: 35%;
+  height: auto;
+  border-radius: 15px;
+  object-fit: contain;
+}
 
-  .topics h1 {
-    color: #F0F0FA;
-    font-size: 2.9375rem;
-    margin: 35px auto;
-    max-width: 800px;
-    line-height: 1.2; 
-    text-align: center;
-    overflow: hidden; 
-  }
+.topics {
+  background-color: #111C41;
+  width: 100%;
+  padding-bottom: 90px;
+  display: flex; 
+  flex-direction: column; 
+  text-align: flex-start; 
+}
 
-  .topics h3 {
-    color:#F0F0FA;
-    font-size: 40px;
-    font-family: 'Onest', sans-serif;
-    margin-left: 10%;
-    margin-top: 5%;
-  }   
+.imagens {
+  display: flex;
+  justify-content: center;
+}
 
-  .topics p {
-    color: #F0F0FA;
-    font-family: 'Onest', sans-serif;
-    font-size: 1.3125rem;
-    line-height: 1.5;
-    margin: 0 10%;
-    padding: 0;
-    max-width: auto;
-    overflow: hidden;
-    text-align: justify;
-  }
+.topics h1 {
+  color: #F0F0FA;
+  font-size: 3rem;
+  margin: 35px auto;
+  max-width: 800px;
+  line-height: 1.2; 
+  text-align: center;
+  overflow: hidden; 
+}
 
-  .topics span {
-    text-decoration: underline;
-    text-decoration-color: #C898EC;
-    text-decoration-thickness: 6px;
-    border-radius: 15%;
-    text-align: center;
-    font-weight: bold;
-    color: #C898EC;
-  }
+.topics h3 {
+  color:#F0F0FA;
+  font-size: 40px;
+  margin-left: 10%;
+  margin-top: 5%;
+}   
 
-  .div{
-    display: flex;
-    flex-direction: row;
-  }
+.topics p {
+  color: #F0F0FA;
+  font-size: 1.3125rem;
+  line-height: 1.5;
+  margin: 0 10%;
+  padding: 0;
+  max-width: auto;
+  overflow: hidden;
+  text-align: justify;
+}
 
-  .imagem1{
-    margin-top: 40px;
-    margin-left: 70px;
-    width: 450px;
-    height: 600px;
-  }
-  .imagem2, .imagem3{
-    margin-top: 40px;
-    margin-left: 100px;
-    width: 270px;
-    height: 600px;
-  }
+.topics span {
+  text-decoration: underline;
+  text-decoration-color: #C898EC;
+  text-decoration-thickness: 6px;
+  border-radius: 15%;
+  text-align: center;
+  font-weight: bold;
+  color: #C898EC;
+}
+
+.imagem1{
+  margin-top: 40px;
+  margin-left: 70px;
+  width: 450px;
+  height: 600px;
+}
+.imagem2, .imagem3{
+  margin-top: 40px;
+  margin-left: 100px;
+  width: 270px;
+  height: 600px;
+}
+
+
 </style>
